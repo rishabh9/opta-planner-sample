da745d28730c78133faf7c916f1edf4848a5a6e4
Rishabh Joshi <rishabh9@gmail.com>
2015-06-19 16:29:04 +0530

    Add sample for DistanceMatrix API.

--- distance-matrix/src/main/java/in/notwork/App.java
+++ distance-matrix/src/main/java/in/notwork/App.java
@@ -1,10 +1,41 @@
 package in.notwork;
 
+import com.google.gson.Gson;
+import com.google.maps.DistanceMatrixApi;
+import com.google.maps.DistanceMatrixApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DistanceMatrix;
+import com.google.maps.model.Unit;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.SocketAddress;
+
 /**
  * Hello world!
  */
 public class App {
+
+    public static final String API_KEY = "AIzaSyCkjTXp7I6gaA0zg5iJpX39nQw4oiYw77s";
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        String[] origins = {"12.9752265,77.7391331"};
+        String[] destinations = {"12.9752265,77.7391331"};
+
+        GeoApiContext context = new GeoApiContext().setApiKey(API_KEY);
+        SocketAddress address = new InetSocketAddress("stbeproxy.symphonyteleca.com", 8080);
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, address);
+        context.setProxy(proxy);
+        DistanceMatrixApiRequest request = DistanceMatrixApi.getDistanceMatrix(context, origins, destinations);
+        try {
+            DistanceMatrix matrix = request.units(Unit.METRIC).await();
+            Gson gson = new Gson();
+            System.out.println(gson.toJson(matrix));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
